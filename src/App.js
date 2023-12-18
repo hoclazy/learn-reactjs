47a3fe3cf1bf32bb1f0769bb93382c8181c8cd12
hoctd.dev <hoctd.dev@gmail.com>
2023-12-18 17:17:23 +0700

    hoctd commit

--- src/App.js
+++ src/App.js
@@ -2,21 +2,45 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    const name = 'Học';
+    const age = 28;
+    const isMale = true;
+    const student = {
+        name: 'Easy Frontend'
+    }
+    const colorList = ['red', 'green', 'blue']
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Em chào anh Học ạ!!!!
-        </p>
-        {/*<a*/}
-        {/*  className="App-link"*/}
-        {/*  href="https://reactjs.org"*/}
-        {/*  target="_blank"*/}
-        {/*  rel="noopener noreferrer"*/}
-        {/*>*/}
-        {/*  Learn React*/}
-        {/*</a>*/}
+
+          {/*render các thẻ*/}
+        <p>Xin chào {isMale ? 'anh' : 'chị'} {name} - {age} tuổi - Giới tính: {isMale ? 'Nam' : 'Nữ'}</p>
+
+{/*          render 1 thẻ theo 1 điều kiện
+          {isMale ? <p>Female</p> : <p>male</p>}
+          {isMale && <p>Female</p> }
+          {!isMale && <p>male</p>}*/}
+
+
+          {/*render nhiều thẻ theo 1 điều kiện*/}
+          {isMale && (
+              <>
+                  <p>Quê quán: Yên Nhân, Ý Yên, Nam Định</p>
+                  <p>Số điện thoại: 0335532246</p>
+                  <p>Email: hoctd.dev@gmail.com</p>
+              </>
+          )}
+
+          {/*render object (lưu ý k render được trực tiếp object mà render các thuộc tính của object)*/}
+          <p>{student.name}</p>
+
+          {/*render list*/}
+          <ul>
+              {colorList.map(color => (
+                  <li key={color} style={{color}}>{color}</li>
+              ))}
+          </ul>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -11,6 +11,11 @@ root.render(
   </React.StrictMode>
 );
 
+//nhiệm vụ:khởi tạo root và render App ở chế độ StrictMode vào root (root này ở index.html)
+// chỉ khởi tạo 1 lần ở đây
+// có thể hiểu là hàm khởi tạo để chạy project Reactjs, nằm trong file index.ts
+// tương tự hàm khởi tạo để chạy 1 project Angular sẽ nằm trong file main.ts
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
